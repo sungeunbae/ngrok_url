3dc7009b3927f3a6c08e0a7f0f4df1a4fea89815
Sung Bae <sungbae@ringo.local>
2021-04-24 17:23:37 +1200

    Added SCP support

--- client/get_ngrok_url.py
+++ client/get_ngrok_url.py
@@ -1,14 +1,26 @@
 import gspread
 from os.path import join, dirname,realpath
 from oauth2client.service_account import ServiceAccountCredentials
-fromo config import google_credential_file
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name(join(dirname(realpath(__file__)),google_credential_file), scope)
-client = gspread.authorize(creds)
-sheet = client.open("Rpi Ngrok URL").sheet1
+import argparse
 
-url=sheet.acell('A1').value
-#print(url)
-new_url, port = url.split("//")[1].split(":")
-print("pi@{} -p {}".format(new_url, port))
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name(join(dirname(realpath(__file__)),'vejgarden-4efd839296c1.json'), scope)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument("--scp",action="store_true", help="Print url and port to be used for scp")
+
+    args = parser.parse_args()
+
+    client = gspread.authorize(creds)
+    sheet = client.open("Rpi Ngrok URL").sheet1
+
+    url=sheet.acell('A1').value
+    #print(url)
+    new_url, port = url.split("//")[1].split(":")
+    if args.scp:
+        print("-P {} pi@{}".format(port, new_url))
+    else:
+        print("pi@{} -p {}".format(new_url, port))
 
